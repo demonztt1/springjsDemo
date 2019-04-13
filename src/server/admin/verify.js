5881f2d4d4a03f19ea3809becee38394c13e9dc0
yan <276852953@qq.com>
2019-04-13 20:00:19 +0800

    整理目录结构，顺便使用import。

---
+++ src/server/admin/verify.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+//var jwt = require('jsonwebtoken');
+//import jwt from 'jsonwebtoken'
+var secretkey = 'secretkey';
+
+router.use(function(req,res,next){
+    if(req.url !='/user/login' && req.url !='/user/register'){
+        //token可能存在post请求和get请求
+        let token = req.body.token || req.query.token || req.headers.token;
+/*
+        jwt.verify(token,secretkey,function(err,decode){
+            if(err){
+                res.json({
+                    message: 'token过期，请重新登录',
+                    resultCode: '403'
+                })
+            }else{
+                next();
+            }
+        })
+*/
+    }else{
+        next();
+    }
+})
+
+
+module.exports = router;
---
+++ src/server/dao/user/userDao.js
@@ -0,0 +1,61 @@
+import  connection from  '../../../config/mysqldb'
+
+
+
+
+class  userDao{
+    constructor(){
+        this.connection;
+    }
+
+
+    add(user){
+        let  addSql = 'INSERT INTO sys_user ( name,pwd ) VALUES( ?,?)';
+        //解析请求参数
+        var params = URL.parse(req.url, true).query;
+        var addSqlParams = [ user.name ,user.pwd];
+
+        //增
+        connection.query(addSql,addSqlParams,function (err, result) {
+            if(err){
+                console.log('[INSERT ERROR] - ',err.message);
+                return;
+            }
+        });
+    }
+
+    findById(id){
+
+    }
+
+    findAll(){
+        let  sql = 'SELECT * FROM sys_user';
+        //查
+        connection.query(sql,function (err, result) {
+            if(err){
+                console.log('[SELECT ERROR] - ',err.message);
+                return;
+            }
+            //    console.log(params.id);
+
+            //把搜索值输出
+            return result;
+        });
+    }
+
+    edit(user){
+
+        let  editSql = 'UPDATE sys_user SET  `name`=?  WHERE id = ?';
+        //解析请求参数
+        let params = URL.parse(req.url, true).query;
+        let editSqlParams = [ user.name,user.id ];
+
+        //增
+        connection.query(editSql,editSqlParams,function (err, result) {
+            if(err){
+                console.log('[UPDATE  ERROR] - ',err.message);
+                return;
+            }
+        });
+    }
+}
